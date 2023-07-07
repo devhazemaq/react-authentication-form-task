6bc718ad3f51784aa59be5441cb39038002de38d
devhazemaq <hazemahmedqindeel@gmail.com>
2023-07-07 15:00:04 +0300

    The initial structure for creating the account for the site, and it will include the logo, quote, and vector

---
+++ src/pages/SignupPage/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class SignupPage extends Component {
+  render() {
+    return (
+      <div className='signup__contain'>
+          <div className="signup__show">
+            <img src="" alt="" />
+            <div className="signup__show__info"> 
+              {/* هاد عشان ألم فيه اللإقتباس والفكتور  */}
+
+            </div>
+          </div>
+      </div>
+    )
+  }
+}
