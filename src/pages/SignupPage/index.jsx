97fa7f73e5840989f5de8a7fdd00bafb46720ad4
devhazemaq <hazemahmedqindeel@gmail.com>
2023-07-07 21:01:04 +0300

    The third structure, the section on the left, contains an image and a

--- src/pages/SignupPage/index.jsx
+++ src/pages/SignupPage/index.jsx
@@ -10,18 +10,23 @@ export default class SignupPage extends Component {
   render() {
     // const IMAGE_BACKG = '/assets/images/Image-Backeground.png';
     const LOGO_2 = '/assets/images/Logo-2.png';
+    const SHAPE_1 = '/assets/images/Vector-Shape-1.png'
 
     return (
       <div className='signup__contain'>
           <div className="signup__show">
-            <img src={LOGO_2} alt="LOGO_2" />
-            <div className="signup__show__info"> 
-              {/* هاد عشان ألم فيه اللإقتباس والفكتور  */}
-              
-              <Quote quoteColor />
-              
 
+            <div className="show__cont">
+              <img src={LOGO_2} alt="LOGO_2" className='logo__2' />
+
+              <div className="signup__show__info"> 
+                {/* هاد عشان ألم فيه اللإقتباس والفكتور  */}
+                
+                <Quote quoteColor />
+                <img src={SHAPE_1} alt="SHAPE_1" />
+              </div>
             </div>
+            
           </div>
       </div>
     )
